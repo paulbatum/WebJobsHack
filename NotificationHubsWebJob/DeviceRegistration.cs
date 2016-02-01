7b1f5cfea73096be3961d8eee5ee9ab5eb6bb0a2
Paul Batum <paul.batum@gmail.com>
2016-01-31 23:10:47 -0800

    Working on device registration

---
+++ NotificationHubsWebJob/DeviceRegistration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure.NotificationHubs;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationHubsWebJob
+{
+    public class DeviceRegistration
+    {
+        
+        public static async Task Register([WebHookTrigger] Installation installation, TraceWriter trace)
+        {
+            var nhConnectionString = ConfigurationManager.ConnectionStrings["MS_NotificationHubConnectionString"].ConnectionString;
+
+            // Create the notification hub client.
+            NotificationHubClient hub = NotificationHubClient
+                .CreateClientFromConnectionString(nhConnectionString, "pbjobs");
+
+            await hub.CreateOrUpdateInstallationAsync(installation);
+            trace.Info("Registration created");
+        }
+    }
+}
---
+++ NotificationHubsWebJob/DeviceSend.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Azure.NotificationHubs;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace NotificationHubsWebJob
+{
+    public class DeviceSend
+    {
+        public static async Task SendNotification([WebHookTrigger] string message, TraceWriter trace)
+        {
+            var nhConnectionString = ConfigurationManager.ConnectionStrings["MS_NotificationHubConnectionString"].ConnectionString;
+
+            // Create the notification hub client.
+            NotificationHubClient hub = NotificationHubClient
+                .CreateClientFromConnectionString(nhConnectionString, "pbjobs");
+
+            // Define a WNS payload
+            var windowsToastPayload = @"<toast><visual><binding template=""ToastText01""><text id=""1"">"
+                                    + message + @"</text></binding></visual></toast>";
+            try
+            {
+                // Send the push notification.
+                var result = await hub.SendWindowsNativeNotificationAsync(windowsToastPayload);
+
+                // Write the success result to the logs.
+                trace.Info(result.State.ToString());
+            }
+            catch (System.Exception ex)
+            {
+                // Write the failure result to the logs.
+                trace.Error(ex.Message, null, "Push.SendAsync Error");
+            }
+        }
+    }
+}
