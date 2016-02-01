fd19afa6cc2453a554ab00842d33d4723c73236e
Paul Batum <paul.batum@gmail.com>
2016-01-31 23:13:26 -0800

    Keys

---
+++ UniversalClientApp/Keys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalClientApp
+{
+    public static class Keys
+    {
+        public static string KuduAuthorization = "";
+    }
+}
