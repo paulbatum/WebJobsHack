6922953b7e316b4960ad5be5dc0d37be0f547468
Paul Batum <paul.batum@gmail.com>
2016-02-19 13:12:31 -0800

    update webhook function

--- NotificationHubsWebJob/DeviceSend.cs
+++ NotificationHubsWebJob/DeviceSend.cs
@@ -14,34 +14,13 @@ namespace NotificationHubsWebJob
 {
     public class DeviceSend
     {
-        public static async Task SendNotification([WebHookTrigger] string message, TraceWriter trace)
+        public static void SendNotification([WebHookTrigger] string message, [NotificationHub] out Notification notification)
         {
-            var nhConnectionString = ConfigurationManager.ConnectionStrings["MS_NotificationHubConnectionString"].ConnectionString;
-
-            // Create the notification hub client.
-            NotificationHubClient hub = NotificationHubClient
-                .CreateClientFromConnectionString(nhConnectionString, "pbjobs");
-
-            // Define a WNS payload
-            var windowsToastPayload = @"<toast><visual><binding template=""ToastText01""><text id=""1"">"
-                                    + message + @"</text></binding></visual></toast>";
-            try
-            {
-                // Send the push notification.
-                var result = await hub.SendWindowsNativeNotificationAsync(windowsToastPayload);
-
-                // Write the success result to the logs.
-                trace.Info(result.State.ToString());
-            }
-            catch (System.Exception ex)
-            {
-                // Write the failure result to the logs.
-                trace.Error(ex.Message, null, "Push.SendAsync Error");
-            }
+            notification = new TemplateNotification(new Dictionary<string, string> { ["message"] = message });
         }
 
         
-        public static void CronJob([TimerTrigger("*/15 * * * * *")] TimerInfo timerInfo, [NotificationHub] out Notification notification)
+        public static void CronJob([TimerTrigger("00:30:00")] TimerInfo timerInfo, [NotificationHub] out Notification notification)
         {
             notification = new TemplateNotification(new Dictionary<string, string> { ["message"] = "foo" });
         }
